45e75e6eadda8d58ba648d2006cddf008acbdd01
Bálint Áts <atsbalint@gmail.com>
2020-04-15 15:59:03 +0200

    init commit

---
+++ online_digit_recognizer_backend/Controllers/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace online_digit_recognizer_backend.Controllers
+{
+    public class test
+    {
+    }
+}
