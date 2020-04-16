ba4b07d77241a6d6405bff21b8e6fb15b67c35cc
Bálint Áts <atsbalint@gmail.com>
2020-04-16 13:55:00 +0200

    create DigitController

---
+++ online_digit_recognizer_backend/Controllers/DigitController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace online_digit_recognizer_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DigitController : ControllerBase
+    {
+        // GET: api/Digit
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Digit/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Digit
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Digit/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
